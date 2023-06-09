dcaf2eca09d377ea46feac1d198fcd07715f8bf0
unknown <pelcastrevargasz@gmil.com>
2023-06-09 14:00:30 -0600

    04 Operadores

---
+++ operadores.py
@@ -0,0 +1,18 @@
+#Operadores Matemáticos 
+# + - * %
+#/ división float
+#// Division entera
+# ** Potencia 
+
+print(2 + 2)
+print(2 + 2)
+print(2 * 2)
+print(2 % 2)
+print(10 / 3)
+print(10 // 3)
+print(3 ** 3)
+
+#Iperadores para Strings
+# + (concatena) * (replica)
+print("Hola" + "Mundo!")
+print("Hola \n" * 4)
