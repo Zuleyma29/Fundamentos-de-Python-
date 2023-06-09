3ccee6baee06a30f02d6c66f8e45924afd82572e
unknown <pelcastrevargasz@gmil.com>
2023-06-09 13:24:14 -0600

    02 Variables

---
+++ variables.py
@@ -0,0 +1,13 @@
+# Python es no Tipado
+
+name = "Zuleyma Pelcastre"
+age = 19
+height = 1.50
+is_student = True
+children = []
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
+print(type(children))
