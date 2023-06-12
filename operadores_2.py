51faddde2df76024fc346cc6a95f127d62d8b031
unknown <pelcastrevargasz@gmil.com>
2023-06-12 10:46:17 -0600

    06 Condicionales

---
+++ operadores_2.py
@@ -0,0 +1,24 @@
+# Operadores booleanos
+# > >=
+print(10>10)
+print(10>=10)
+# < <=
+print(10<10)
+print(10<=10)
+# ==
+print(10==10)
+# !=
+print(10!=10)
+
+
+# Operadores logicos
+# and (True if all True)
+print(True and True)
+print(True and False)
+# or (True if all True)
+print(True or True)
+print(True or False)
+# not (not True)
+print(not True)
+print(not False)
+
