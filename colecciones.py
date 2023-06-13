97aaccccae5d2f13564f51e4f8e0b2a41b063342
unknown <pelcastrevargasz@gmil.com>
2023-06-13 15:00:13 -0600

    08 Listas

---
+++ colecciones.py
@@ -0,0 +1,23 @@
+# Listas
+#Entre corchetes
+my_list = [123,True,3.14,"Hi!"]
+print(type(my_list))
+print(my_list)
+
+
+# Indexing
+print(my_list[0])
+
+# Slicing
+print(my_list[2:])
+
+students = ["Guadalupe","Alicia","Alin"]
+print(students)
+students.append("Paco")
+students.append("Nacho")
+students.append("Gaby")
+print(students)
+students.remove("Guadalupe")
+print(students)
+students[1]= "Vero"
+print(students)
---
+++ crud.py
@@ -0,0 +1,36 @@
+students = [] 
+
+def show_students():
+    for student in students:
+        print(student)
+
+def add_student(student):
+    students.append(student)
+def remove_student(student):
+    try:
+        students.remove(student)
+    except Exception:
+        print("No existe")
+
+choice_text='''
+Elige una opcion:
+1 - Insertar
+2 - Mostrar
+3 - Eliminar
+4 - Salir
+'''
+while True:
+    choice=int(input(choice_text))
+    if choice == 1:
+        student = input("Ingresa student:")
+        add_student(student)
+    elif choice == 2:
+        show_students()
+    elif choice == 3:
+        student= input("Infresa student a eliminar:")
+        remove_student(student)
+    elif choice == 4:
+        break
+    else:
+            print('Incorrecto Option!')
+
